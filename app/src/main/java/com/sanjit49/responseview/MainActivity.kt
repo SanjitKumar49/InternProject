fd495a982824f7eff0a3fb4a3201c8fd80f6f1a3
Sanjit Kumar <kumarsanjit18779@gmail.com>
2023-10-06 02:52:38 +0530

    sort demo

---
+++ app/src/main/java/com/sanjit49/responseview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sanjit49.responseview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.setTitle("MEAT PACKING")
+    }
+}
